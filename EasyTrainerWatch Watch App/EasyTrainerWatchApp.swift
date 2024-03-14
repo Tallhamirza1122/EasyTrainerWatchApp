46d86ed66aa5fba6937aacae587e38e8e28e4449
Talha <Talha mirza>
2024-03-14 22:23:12 +0500

    Initial Commit

---
+++ EasyTrainerWatch Watch App/EasyTrainerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyTrainerWatchApp.swift
+//  EasyTrainerWatch Watch App
+//
+//  Created by MacBook Pro on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyTrainerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
